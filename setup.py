1d309a37e455b8a77748f59721f05bb94f095342
Nolan Woods <nolan_w@sfu.ca>
2019-01-16 15:55:27 -0800

    Restructure for setuptools

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from feature_merge.__version import __versionstr__
+
+with open('README.rst') as readme:
+    setup(
+        name='feature_merge',
+        version=__versionstr__,
+        packages=find_packages(),
+        long_description=readme.read(),
+        install_requires=['gffutils'],
+        scripts=['feature_merge/feature_merge.py'],
+        url='https://github.com/brinkmanlab/feature_merge',
+        license='MIT',
+        author='Nolan Woods',
+        author_email='nolan_w@sfu.ca',
+        description='Merge overlapping features of GFF/GTF files.',
+        include_package_data=True
+    )
