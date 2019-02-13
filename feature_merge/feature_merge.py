692bda632185da5091262c2893028fd54266b1b7
Nolan Woods <nolan_w@sfu.ca>
2019-02-12 18:55:50 -0800

    Add exact only and NONE merge options

--- feature_merge/feature_merge.py
+++ feature_merge/feature_merge.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
+import shutil
+
 import gffutils
 import sys
 import getopt
 
-usage = "Usage: feature_merge.py [-i] [-e] [-v] [-f type[,type..]].. <input1> <input2> [..<input_n>]. Accepts GFF or GTF format.\n" \
+usage = "Usage: feature_merge.py [-i] [-e] [-x] [-v] [-f type[,type..]].. <input1> <input2> [..<input_n>]. Accepts GFF or GTF format.\n" \
         "-v Print version and exit\n" \
-        "-f Comma seperated types of features to merge. Must be terms or accessions from the SOFA sequence ontology or \"ALL\". (Can be provided more than once to specify multiple merge groups)\n" \
+        "-f Comma seperated types of features to merge. Must be terms or accessions from the SOFA sequence ontology, \"ALL\", or \"NONE\". (Can be provided more than once to specify multiple merge groups)\n" \
         "-i Ignore strand, merge feature regardless of strand\n" \
+        "-x Only merge features with identical coordinates\n" \
         "-e Exclude component features from output"
 
-def merge(target_db, features, ignore_strand=False, ignore_featuretype=False):
+def merge(self, features, exact_only=False, ignore_strand=False, ignore_featuretype=False):
     """
     Merge overlapping features together.
 
@@ -18,10 +21,13 @@ def merge(target_db, features, ignore_strand=False, ignore_featuretype=False):
 
     features : iterator of Feature instances
 
+    exact_only : bool
+        If True, will only merge features with identical coordinates;
+        otherwise will merge any overlapping or contiguous features.
+
     ignore_strand : bool
         If True, features on multiple strands will be merged, and the final
-        strand will be set to '.'.  Otherwise, ValueError will be raised if
-        trying to merge features on differnt strands.
+        strand will be set to '.'
 
     ignore_feauretype : bool
         If True, features of multiple types will be merged, and the final
@@ -35,17 +41,24 @@ def merge(target_db, features, ignore_strand=False, ignore_featuretype=False):
 
     # To start, we create a merged feature of just the first feature.
     features = iter(features)
-    feature = next(features)
+    try:
+        feature = next(features)
+    except StopIteration:
+        return
     current_merged_start = feature.start
     current_merged_stop = feature.stop
-    seqid = feature.seqid
+    current_merged_seqid = feature.seqid
     strand = feature.strand
     featuretype = feature.featuretype
     feature_components = [feature]
+    if exact_only:
+        coordinate_criteria = lambda seqid, start, stop: start == current_merged_start and stop == current_merged_stop and seqid == current_merged_seqid
+    else:
+        coordinate_criteria = lambda seqid, start, stop: start <= current_merged_stop + 1 and seqid == current_merged_seqid
 
     for feature in features:
         # Does this feature start within the currently merged feature?...
-        if feature.start <= current_merged_stop + 1 and feature.seqid == seqid and (ignore_strand or feature.strand == strand) and (ignore_featuretype or feature.featuretype == featuretype):
+        if coordinate_criteria(feature.seqid, feature.start, feature.stop) and (ignore_strand or feature.strand == strand) and (ignore_featuretype or feature.featuretype == featuretype):
             feature_components.append(feature)
             # ...It starts within, so leave current_merged_start where it
             # is.  Does it extend any farther?
@@ -59,16 +72,18 @@ def merge(target_db, features, ignore_strand=False, ignore_featuretype=False):
         else:
             # The start position is outside the merged feature, so we're
             # done with the current merged feature.  Prepare for output...
-            yield target_db._feature_returner(
-                seqid=seqid,
+            attributes={}
+            for component in feature_components: attributes = gffutils.helpers.merge_attributes(attributes, component.attributes)
+            yield self._feature_returner(
+                seqid=current_merged_seqid,
                 source=",".join(set(component.source for component in feature_components)),
                 featuretype="sequence_feature" if ignore_featuretype else featuretype,
                 start=current_merged_start,
                 end=current_merged_stop,
                 score='.',
                 strand='.' if ignore_strand else strand,
-                frame='.',
-                attributes=''), feature_components
+                frame='.' if not all(map(lambda x: x.frame == feature_components[0].frame, feature_components)) else feature_components[0].frame,
+                attributes=attributes), feature_components
 
             # and we start a new one, initializing with this feature's
             # start and stop.
@@ -79,8 +94,10 @@ def merge(target_db, features, ignore_strand=False, ignore_featuretype=False):
             featuretype = feature.featuretype
             feature_components = [feature]
 
-    yield target_db._feature_returner(
-        seqid=seqid,
+    attributes = {}
+    for component in components: attributes = gffutils.helpers.merge_attributes(attributes, component.attributes)
+    yield self._feature_returner(
+        seqid=current_merged_seqid,
         source = ",".join(set(component.source for component in feature_components)),
         featuretype="sequence_feature" if ignore_featuretype else featuretype,
         start=current_merged_start,
@@ -88,11 +105,49 @@ def merge(target_db, features, ignore_strand=False, ignore_featuretype=False):
         score='.',
         strand='.' if ignore_strand else strand,
         frame='.',
-        attributes=''), feature_components
+        attributes=attributes), feature_components
+
+
+def update(self, data, **kwargs):
+    """
+    Ripped this out of FeatureDB.update() to deal with a bug.
+    Update database with features in `data`.
+
+    data : str, iterable, FeatureDB instance
+        If FeatureDB, all data will be used. If string, assume it's
+        a filename of a GFF or GTF file.  Otherwise, assume it's an
+        iterable of Feature objects.  The classes in gffutils.iterators may
+        be helpful in this case.
+    """
+    from gffutils import create
+    from gffutils import iterators
+
+    # Handle all sorts of input
+    data = iterators.DataIterator(data)
+
+    if self.dialect['fmt'] == 'gtf':
+        if 'id_spec' not in kwargs:
+            kwargs['id_spec'] = {'gene': 'gene_id', 'transcript': 'transcript_id'}
+        db = create._GTFDBCreator(
+            data=data, dbfn=self.dbfn, dialect=self.dialect, **kwargs)
+    elif self.dialect['fmt'] == 'gff3':
+        if 'id_spec' not in kwargs:
+            kwargs['id_spec'] = 'ID'
+        db = create._GFFDBCreator(
+            data=data, dbfn=self.dbfn, dialect=self.dialect, **kwargs)
+    else:
+        raise ValueError
+
+    db._autoincrements.update(self._autoincrements)
+    db._populate_from_lines(data)
+    db._update_relations()
+    db._finalize()
+    self._autoincrements.update(db._autoincrements)
 
 if __name__ == '__main__':
     ignore_strand = False
     ignore_featuretypes = False
+    exact_only = False
     exclude_components = False
     featuretypes_groups = []
 
@@ -108,8 +163,10 @@ if __name__ == '__main__':
             elif opt == '-e':
                 exclude_components = True
             elif opt == '-f':
-                ignore_featuretypes = True
-                if val != "ALL": featuretypes_groups.append(tuple(filter(None, val.split(','))))
+                if val != "NONE": ignore_featuretypes = True
+                if val != "ALL": featuretypes_groups.append(set(filter(None, val.split(','))))
+            elif opt == '-x':
+                exact_only = True
 
     except getopt.GetoptError as err:
         args = []
@@ -127,17 +184,23 @@ if __name__ == '__main__':
         merge_order = ('seqid', 'featuretype', 'strand', 'start')
 
     #Load input data
-    db = gffutils.create_db(args[0], ":memory:", merge_strategy="merge")
-    for input in args[2:]:
-        db = db.update(input, make_backup=False)
+    db = gffutils.create_db(args[0], ":memory:", merge_strategy="create_unique")
+
+    for input in args[1:]:
+        update(db, input, merge_strategy="create_unique")
 
     remaining_featuretypes = set(db.featuretypes())
 
+    #Output header
+    print("##gff-version 3")
+
     #Merge features per featuregroup
     for featuregroup in featuretypes_groups:
-        if featuregroup: remaining_featuretypes -= featuregroup
-        else: remaining_featuretypes = set()
-        for merged, components in merge(db, db.all_features(featuretype=featuregroup, order_by=merge_order), ignore_strand, ignore_featuretypes):
+        if featuregroup:
+            remaining_featuretypes -= featuregroup
+        else:
+            remaining_featuretypes = set()
+        for merged, components in merge(db, db.all_features(featuretype=featuregroup, order_by=merge_order), exact_only, ignore_strand, ignore_featuretypes):
             # Store merged record components as hierarchy
             merged.id = "merged"
             if len(components) > 1:
@@ -151,7 +214,7 @@ if __name__ == '__main__':
                 if len(merged.id) > 32:
                     merged.id = hex(hash(merged.id)+ 2**63)[2:]
 
-                merged.attributes["ID"] = merged.id
+                merged.attributes["ID"] = [merged.id]
 
                 # Set merged record as component parent
                 for component in components:
@@ -162,6 +225,7 @@ if __name__ == '__main__':
             #Output components
             if len(components) == 1 or not exclude_components:
                 for component in components:
+                    component.attributes["ID"] = component.attributes.get('ID', [component.id])
                     print(component)
 
             #Output merged record if more than one component
