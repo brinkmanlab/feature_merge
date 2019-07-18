f47b9fb9b31317c6f74a681ee1bc251c8b07f4f2
Nolan Woods <nolan_w@sfu.ca>
2019-07-18 11:51:20 -0700

    Change to pbr

--- setup.py
+++ setup.py
@@ -1,23 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-import re
-with open('feature_merge/__version.py') as version_file:
-    __versionstr__ = '.'.join(re.search(r"__version__ = \[(\d+), (\d+), (\d+)\]", version_file.read(), re.M).group()[1:])
-
-with open('README.rst') as readme:
-    setup(
-        name='feature_merge',
-        version=__versionstr__,
-        packages=find_packages(),
-        long_description=readme.read(),
-        install_requires=['gffutils'],
-        scripts=['bin/feature_merge'],
-        url='https://github.com/brinkmanlab/feature_merge',
-        license='MIT',
-        author='Nolan Woods',
-        author_email='nolan_w@sfu.ca',
-        description='Merge overlapping features of GFF/GTF files.',
-        include_package_data=True,
-        test_suite='tests',
-    )
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+)
